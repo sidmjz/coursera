08b1106891aba8aa42970b99a1c766567607128a
Sid Zhang <mingjian.zhang@hotmail.com>
2020-09-12 19:26:53 +0800

    Lesson 57

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-(function () {
-  console.log("Hello Coursera!");
-  return 1;
-})();
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
 
-console.log(a);
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
+
+        
+      });
+  }
+);
+
+
+
+
